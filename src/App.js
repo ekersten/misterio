885df2dd98baf3f06c9a56d721e0cb1554326896
Eric Kersten <ekersten@gmail.com>
2021-01-05 16:24:40 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup'
+
+
+import data from './data';
 
 function App() {
+  
+  const [monsters, setMonsters] = useState(data.monsters);
+  const [victims, setVictims] = useState(data.victims);
+  const [rooms, setRooms] = useState(data.rooms);
+
+  const enabledMonsters = monsters.filter(monster => monster.enabled);
+  const enabledVictims = victims.filter(victim => victim.enabled);
+  const enabledRooms = rooms.filter(room => room.enabled);
+
+  const toggleRoom = room => {
+    setRooms(prevRooms => {
+      const newRooms = prevRooms.filter(item => item.id !== room.id);
+      const roomCopy = {...room};
+      roomCopy.enabled = !roomCopy.enabled;
+
+      return [
+        ...newRooms,
+        roomCopy
+      ];
+    })
+  }
+
+  const toggleVictim = victim => {
+    setVictims(prevVictims => {
+      const newVictims = prevVictims.filter(item => item.id !== victim.id);
+      const victimCopy = { ...victim };
+      victimCopy.enabled = !victimCopy.enabled;
+
+      return [
+        ...newVictims,
+        victimCopy
+      ];
+    })
+  }
+
+  const toggleMonster = monster => {
+    setMonsters(prevMonsters => {
+      const newMonsters = prevMonsters.filter(item => item.id !== monster.id);
+      const monsterCopy = { ...monster };
+      monsterCopy.enabled = !monsterCopy.enabled;
+
+      return [
+        ...newMonsters,
+        monsterCopy
+      ];
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Accordion>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Monstruos
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                    <ListGroup>
+                    {monsters.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map(monster => (
+                        <ListGroup.Item key={`monster-${monster.id}`}>
+                        <Form.Check type="checkbox" id={`monster-${monster.id}-checkbox`} checked={monster.enabled} label={monster.name} onChange={() => toggleMonster(monster)} />
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="1">
+                Víctimas
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <ListGroup>
+                    {victims.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map(victim => (
+                      <ListGroup.Item key={`victim-${victim.id}`}>
+                        <Form.Check type="checkbox" id={`victim-${victim.id}-checkbox`} checked={victim.enabled} label={victim.name} onChange={() => toggleVictim(victim)} />
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="2">
+                Habitaciones
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <ListGroup>
+                    {rooms.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map(room => (
+                      <ListGroup.Item key={`room-${room.id}`}>
+                        <Form.Check type="checkbox" id={`room-${room.id}-checkbox`} checked={room.enabled} label={room.name} onChange={() => toggleRoom(room)} />
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="3">
+                { enabledMonsters.length * enabledVictims.length * enabledRooms.length } Combinaciones
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>
+                  <ListGroup>
+                    {monsters.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).filter(monster => monster.enabled).map(monster => (
+                      victims.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).filter(victim => victim.enabled).map(victim => (
+                        rooms.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).filter(room => room.enabled).map(room => (
+                          <ListGroup.Item key={`combination-${monster.id}-${victim.id}-${room.id}`}>
+                            {monster.name} mató a {victim.name} en {room.name}
+                          </ListGroup.Item>
+                        ))
+                      ))
+                    ))}
+                  </ListGroup>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = {
+    monsters: [
+        {id: 1, name: 'Drácula', enabled: true},
+        {id: 2, name: 'Frankenstein', enabled: true},
+        {id: 3, name: 'Hombre Lobo', enabled: true},
+        {id: 4, name: 'Fantasma', enabled: true},
+        {id: 5, name: 'Momia', enabled: true},
+        {id: 6, name: 'Dr. Jeckyll Mr. Hyde', enabled: true},
+    ],
+    victims: [
+        {id: 1, name: 'Conde', enabled: true},
+        {id: 2, name: 'Condesa', enabled: true},
+        {id: 3, name: 'Ama de llaves', enabled: true},
+        {id: 4, name: 'Mayordomo', enabled: true},
+        {id: 5, name: 'Doncella', enabled: true},
+        {id: 6, name: 'Jardinero', enabled: true},
+    ],
+    rooms: [
+        {id: 1, name: 'Biblioteca', enabled: true},
+        {id: 2, name: 'Alcoba', enabled: true},
+        {id: 3, name: 'Cochera', enabled: true},
+        {id: 4, name: 'Vestíbulo', enabled: true},
+        {id: 5, name: 'Bodega', enabled: true},
+        {id: 6, name: 'Panteón', enabled: true},
+        {id: 7, name: 'Laboratorio', enabled: true},
+        {id: 8, name: 'Salón', enabled: true},
+    ]
+}
+
+export default data;
